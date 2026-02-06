63ab846a2088bf62782b99295bc0e72392bfd224
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2026-02-06 18:13:31 +0530

    Update CP_general_tips.cpp

--- cpp/CP_general_tips.cpp
+++ cpp/CP_general_tips.cpp
@@ -1,4 +1,10 @@
-In map, you don't have to do:- 
+//In map, you don't have to do:- 
 if (elem in map) map[elem]++;
 else map[elem] = 0;
-You can just do map[elem]++. It automatically initializes to 0 if not found.
+//You can just do 
+map[elem]++      //It automatically initializes to 0 if not found.
+
+//If there are 3 ints a, b and c; and we want to check if (a > b*c) or something
+//If b*c is too high to store in an int, it will give runtime error saying 'integer overflow'
+//To fix this, we don't have to change b and c to long long. We can just do
+if (a > (long long) b*c) return -1;
